bba13913a812a0e8dd93eeba64608dd6cc4b7c68
robinjac <robinjac@kth.se>
2017-03-15 14:56:30 +0100

    order 2 chain

--- js/questionCtrl.js
+++ js/questionCtrl.js
@@ -1,7 +1,7 @@
 
 trumpOrDumpApp.controller('QuestionCtrl',function($scope, Trump){
 	
-	var tweets = Trump.getTweets(5); //training data for the markov chain
+	var tweets = Trump.getTweets(10); //training data for the markov chain
 	
 	var q = Math.round(Math.random()) //q = 1 real tweet, q = 0 false tweet
 	
@@ -34,6 +34,9 @@ trumpOrDumpApp.controller('QuestionCtrl',function($scope, Trump){
 			tweet.push(word);
 			if (tweet.length > min_length && terminals.hasOwnProperty(word)) 
 				break;
+			
+			if(tweet.length > 30) //maximum tweet length of 20 words
+				break;
 		}
 		if (tweet.length < min_length) 
 			return generateFakeTweet(min_length);
@@ -43,7 +46,7 @@ trumpOrDumpApp.controller('QuestionCtrl',function($scope, Trump){
 	
 	$scope.getQuestion = function(){
 	
-		if(!markov && tweets.length > 4){
+		if(!markov && tweets.length > 9){ //makes sure we load all the 7 tweets
 			markov = true; //makes sure this loop is run only once
 			
 			//this for-loop builds the markov chain
@@ -53,10 +56,14 @@ trumpOrDumpApp.controller('QuestionCtrl',function($scope, Trump){
 				startwords.push(words[0]);
 				
 				for (var j = 0; j < words.length - 1; j++) {
-					if (wordstats.hasOwnProperty(words[j])) {
+					if (wordstats.hasOwnProperty(words[j]) && wordstats.hasOwnProperty(words[j+1])) {
 						wordstats[words[j]].push(words[j+1]);
+						wordstats[words[j+1]].push(words[j+2]);
+						//wordstats[words[j+2]].push(words[j+3]);
 					} else {
 						wordstats[words[j]] = [words[j+1]];
+						wordstats[words[j+1]] = [words[j+2]];
+						//wordstats[words[j+2]] = [words[j+3]];
 					}
 				}
 			}
@@ -66,7 +73,7 @@ trumpOrDumpApp.controller('QuestionCtrl',function($scope, Trump){
 				question = tweets[0];
 			}else{
 				Trump.setAnswer(false);
-				question = generateFakeTweet(3); //generates a new tweet with min length of 3 words
+				question = generateFakeTweet(4); //generates a new tweet with min length of 3 words
 			};
 
 			return question; 
