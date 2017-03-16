f1b9190c1a1066ce2b3d9167995c0d9758269154
robinjac <robinjac@kth.se>
2017-03-16 15:20:53 +0100

    Update displayCtrl.js

--- js/displayCtrl.js
+++ js/displayCtrl.js
@@ -3,11 +3,7 @@ trumpOrDumpApp.controller('DisplayCtrl',function($scope, Trump){
 	
 	$scope.score = Trump.getScore();
 	
-<<<<<<< HEAD:js/DisplayCtrl.js
 	$scope.reset = function(){
 		Trump.updateScore(0);
 	};
 });
-=======
-});
->>>>>>> origin/master:js/displayCtrl.js
